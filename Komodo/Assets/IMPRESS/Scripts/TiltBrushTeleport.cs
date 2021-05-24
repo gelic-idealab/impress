e1e5f8e925b9f7c3602ed97374f6bb1169ca76bf
bcdang2 <bcdang2@illinois.edu>
2021-05-24 15:42:07 -0500

    refactored functions and some variable names

--- Komodo/Assets/IMPRESS/Scripts/TiltBrushTeleport.cs
+++ Komodo/Assets/IMPRESS/Scripts/TiltBrushTeleport.cs
@@ -13,9 +13,9 @@ namespace Komodo.IMPRESS
 
         public Vector2 offsetLimits = new Vector2(-0.05f, 0.8f);
 
-        public MeshRenderer animalScaleMaterial;
+        public MeshRenderer animalRulerMesh;
 
-        private LineRenderer lineRenderer;
+        private LineRenderer handToHandLine;
 
         public Transform animalRuler;
 
@@ -25,19 +25,19 @@ namespace Komodo.IMPRESS
         //get parent if we are switching objects between hands we want to keep track of were to place it back, to avoid hierachy parenting displacement
         public Transform originalParentOfFirstHandTransform;
 
-        public Transform[] handReferences = new Transform[2];
+        public Transform[] hands = new Transform[2];
 
-        public Transform pivotRootTransform;
+        public Transform pivotPointsParent;
 
         //coordinate system to use to tilt double grand object appropriately: pulling, pushing, hand lift, and hand lower
-        public Transform doubleGrabRotateCoordinate;
+        public Transform pivotPoint1;
         
         public bool isInitialDoubleGrab;
         
-        private float doubleGrabinitialDistance;
+        private float initialHandDistance;
 
-        private Vector3 doublGrabinitialScale = Vector3.one;
-        
+        private Vector3 initialPlayerScale = Vector3.one;
+
         private Quaternion initialPlayerRotation;
 
         public Action onDoubleTriggerPress;
@@ -58,13 +58,13 @@ namespace Komodo.IMPRESS
 
         float currentTurn;
 
-        Quaternion initialRot;
+        Quaternion initialRotation;
 
         Quaternion invertedRot;
 
         private float currentScale = 1;
 
-        float newinvertedscaleRatioBasedOnDistance = 1;
+        float scaleRatio = 1;
         
         public Transform desktopCamera;
 
@@ -72,10 +72,10 @@ namespace Komodo.IMPRESS
         {
             //create hierarchy to rotate double grab objects appropriately
             //create root parent and share it through scripts by setting it to a static field
-            pivotRootTransform = new GameObject("PIVOT_ROOT").transform;
+            pivotPointsParent = new GameObject("PIVOT_ROOT").transform;
 
             //construct coordinate system to reference for tilting double grab object 
-            doubleGrabRotateCoordinate = new GameObject("DoubleGrabCoordinateForObjectTilt").transform;
+            pivotPoint1 = new GameObject("DoubleGrabCoordinateForObjectTilt").transform;
 
         }
 
@@ -88,36 +88,36 @@ namespace Komodo.IMPRESS
             {
                 if (player.TryGetComponent(out PlayerReferences pR))
                 {
-                    handReferences[0] = pR.handL;
-                    handReferences[1] = pR.handR;
+                    hands[0] = pR.handL;
+                    hands[1] = pR.handR;
                 }
 
                 teleportPlayer = player.GetComponent<TeleportPlayer>();
             }
 
 
-            lineRenderer = gameObject.GetComponent<LineRenderer>();
+            handToHandLine = gameObject.GetComponent<LineRenderer>();
 
-            animalScaleMaterial.material.SetTextureOffset("_MainTex", Vector2.zero);
+            animalRulerMesh.material.SetTextureOffset("_MainTex", Vector2.zero);
 
             animalRuler.gameObject.SetActive(false);
 
-            lineRenderer.enabled = false;// (false);
+            handToHandLine.enabled = false;
 
 
-            onDoubleTriggerPress += SetDoubleTriggerStateOn;
+            onDoubleTriggerPress += StartWorldPulling;
 
-            onDoubleTriggerRelease += SetDoubleTriggerStateOff;//  .AddListener(() => SetDoubleTriggerStateOff());
+            onDoubleTriggerRelease += StopWorldPulling;
         }
 
-        [ContextMenu("Double Grab State ON")]
-        public void SetDoubleTriggerStateOn()
+        [ContextMenu("Start World Pulling")]
+        public void StartWorldPulling()
         {
             isInitialDoubleGrab = false;
 
             animalRuler.gameObject.SetActive(true);
 
-            lineRenderer.enabled = true;
+            handToHandLine.enabled = true;
 
             //register our update loop to be called
             if (GameStateManager.IsAlive)
@@ -126,18 +126,77 @@ namespace Komodo.IMPRESS
             }
         }
 
-        [ContextMenu("Double Grab State OFF")]
-        public void SetDoubleTriggerStateOff()
+        [ContextMenu("Stop World Pulling")]
+        public void StopWorldPulling()
         {
             animalRuler.gameObject.SetActive(false);
 
-            lineRenderer.enabled = false;
+            handToHandLine.enabled = false;
 
             //deregister our update loop not be called when not in use
             if (GameStateManager.IsAlive)
             {
                 GameStateManager.Instance.DeRegisterUpdatableObject(this);
             }
+        }
+
+        public float ComputeNewScaleRatio () 
+        {
+            var handDistance = Vector3.Distance(hands[0].transform.position, hands[1].transform.position);
+
+            scaleRatio = (handDistance / (xrPlayer.localScale.x * initialHandDistance));
+
+            scaleRatio *= initialPlayerScale.x;
+
+            return scaleRatio;
+        }
+
+        public void UpdateRotation (float scaleClamp2) 
+        {
+            pivotPoint1.position = hands[0].transform.position;
+
+            pivotPoint1.rotation = Quaternion.LookRotation(xrPlayer.InverseTransformPoint(hands[1].transform.position - hands[0].transform.position), Vector3.up);
+
+            currentTurn = (pivotPoint1.localEulerAngles.y - initialY);
+            
+            xrPlayer.localRotation = Quaternion.AngleAxis(currentTurn, Vector3.up) * initialRotation;
+        }
+
+        public void UpdateRulerPose (float scale)
+        {
+            animalRuler.position = ((hands[1].transform.position + hands[0].transform.position) / 2);
+
+            animalRuler.localScale = Vector3.one * scale;
+
+            handToHandLine.SetPosition(0, hands[0].transform.position);
+
+            handToHandLine.SetPosition(1, hands[1].transform.position);
+        }
+
+        public void UpdateRulerValue (float newScaleRatio)
+        {
+
+            var rulerValue = ((newScaleRatio - 0.9f) / 1.3f);
+
+            animalRulerMesh.material.SetTextureOffset("_MainTex", new Vector2(Mathf.Clamp(rulerValue, offsetLimits.x, offsetLimits.y), 0));
+        }
+
+        public void InitializeGrabCoordinates ()
+        {
+            //grab values to know how we should start affecting our object 
+            initialHandDistance = Vector3.Distance(hands[0].position, hands[1].position);
+
+            initialPlayerScale = xrPlayer.localScale.x * (Vector3.one * scaleRatio);
+
+            var initialHandDistance2 = Vector3.Distance(hands[0].transform.position, hands[1].transform.position);
+
+            pivotPoint1.position = hands[0].transform.position; 
+            
+            pivotPoint1.rotation = Quaternion.LookRotation(xrPlayer.InverseTransformPoint(hands[1].transform.position - hands[0].transform.position), Vector3.up);
+
+            initialRotation = xrPlayer.localRotation * Quaternion.Inverse(xrPlayer.localRotation);
+            
+            initialY = pivotPoint1.localEulerAngles.y;
         }
 
         public void OnUpdate(float realltime)
@@ -154,65 +213,34 @@ namespace Komodo.IMPRESS
 
             if (isInitialDoubleGrab == false)
             {
-                //inficates to run this only once at start to get initial values to use in update loop
+                //run this only once to get initial values to use in update loop
+                InitializeGrabCoordinates();
+
                 isInitialDoubleGrab = true;
-
-                //grab values to know how we should start affecting our object 
-                doubleGrabinitialDistance = Vector3.Distance(handReferences[0].position, handReferences[1].position);
-
-
-                doublGrabinitialScale = xrPlayer.localScale.x * (Vector3.one * newinvertedscaleRatioBasedOnDistance);
-
-                var handDistance2 = Vector3.Distance(handReferences[0].transform.position, handReferences[1].transform.position);
-
-                doubleGrabRotateCoordinate.position = handReferences[0].transform.position; 
-                
-                doubleGrabRotateCoordinate.rotation = Quaternion.LookRotation(xrPlayer.InverseTransformPoint(handReferences[1].transform.position - handReferences[0].transform.position), Vector3.up);
-
-                initialRot = xrPlayer.localRotation * Quaternion.Inverse(xrPlayer.localRotation);
-                
-                initialY = doubleGrabRotateCoordinate.localEulerAngles.y;
 
                 return;
             }
 
-            //scale
-            var handDistance = Vector3.Distance(handReferences[0].transform.position, handReferences[1].transform.position);
+            // Scale
 
-            newinvertedscaleRatioBasedOnDistance = (handDistance / (xrPlayer.localScale.x * doubleGrabinitialDistance));
+            float newScaleRatio = ComputeNewScaleRatio();
 
-            newinvertedscaleRatioBasedOnDistance *= doublGrabinitialScale.x;
-
-            var rulerValue = ((newinvertedscaleRatioBasedOnDistance - 0.9f) / 1.3f);
-
-            animalScaleMaterial.material.SetTextureOffset("_MainTex", new Vector2(Mathf.Clamp(rulerValue, offsetLimits.x, offsetLimits.y), 0));
+            UpdateRulerValue(newScaleRatio);
 
             if (float.IsNaN(currentEnvironment.localScale.y)) 
             {
                 return;
             }
 
-            var scaleClamp2 = Mathf.Clamp(newinvertedscaleRatioBasedOnDistance, 0.35f, 5f);
+            var scaleClamp2 = Mathf.Clamp(newScaleRatio, 0.35f, 5f);
 
             teleportPlayer.UpdatePlayerScale(scaleClamp2);
 
-            ///rotate
+            // Rotation
 
-            doubleGrabRotateCoordinate.position = handReferences[0].transform.position;
+            UpdateRotation(scaleClamp2);
 
-            doubleGrabRotateCoordinate.rotation = Quaternion.LookRotation(xrPlayer.InverseTransformPoint(handReferences[1].transform.position - handReferences[0].transform.position), Vector3.up);
-
-            currentTurn = (doubleGrabRotateCoordinate.localEulerAngles.y - initialY);
-            
-            xrPlayer.localRotation = Quaternion.AngleAxis(currentTurn, Vector3.up) * initialRot;
-            
-            animalRuler.position = ((handReferences[1].transform.position + handReferences[0].transform.position) / 2);
-
-            animalRuler.localScale = Vector3.one * scaleClamp2;
-
-            lineRenderer.SetPosition(0, handReferences[0].transform.position);
-
-            lineRenderer.SetPosition(1, handReferences[1].transform.position);
+            UpdateRulerPose(scaleClamp2);
         }
     }
 }
